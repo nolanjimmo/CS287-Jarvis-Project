41693c763a195acca956d1cf52c313b85e0a2582
nolanjimmo <barcafan12345@gmail.com>
2021-10-11 11:58:44 -0400

    Jarvis is ready for connection with database
    Jarvis is successfully entering training mode and should be ready for connection with the database. NOTE: I added the instruction filename and the tokens.txt filename to the gitignore file because if we commit the token.txt file gitub automatically shuts down those tokens and we have to get new ones.

--- jarvis.py
+++ jarvis.py
@@ -1,8 +1,16 @@
 # PR01 Jarvis Instantiantion CS287
 # Nolan Jimmo, Tucker Paron, Katey Forsyth, Chris O'Neil
 
-#APP_TOKEN = "xapp-1-A02GFLAUZ0T-2552947742742-90bd24024278ee7f8e78f8b59a2f0fd544ab527688c7b25c2e7d2060dfa81153"
+# TOKENS
 
+with open("tokens.txt", 'r') as infile:
+    lines = infile.readlines()
+infile.close()
+
+API_TOKEN = lines[0]
+APP_TOKEN = lines[1]
+
+# IMPORTS
 import websocket
 import requests
 import json
@@ -14,12 +22,71 @@ except ImportError:
     import _thread as thread
 import time
 
+##Global Variables for training mode switch
+training_time = False # Training Mode Switch
+action_received = False
+
+def send_message(channel, text):
+    data = {
+        'token': API_TOKEN,
+        'channel': channel,
+        'text': text
+        }
+    requests.post(url='https://slack.com/api/chat.postMessage', data=data)
+    
+
 def on_message(ws, message):
+    global training_time
+    global action_received
+    text = ''
+    
+    #This is the user value for when our bot sends messsages
+    #We will be using this to make sure Jarvis doesn't respond to itself
+    sender = 'U02GCL03FSR'
+
+    # Use envelope_id field to send a response of acknowledgement back to Slack.
     message = json.loads(message)
-    envelope_id = message["envelope_id"]
-    resp = {"envelope_id": envelope_id }
-    ws.send(str.encode(json.dumps(resp )))
-    print(message['payload']['event']['text'])
+    envelope_id = message['envelope_id']
+    resp = {'envelope_id': envelope_id}
+    ws.send(str.encode(json.dumps(resp)))
+
+    channel = message['payload']['event']['channel']
+    text = message['payload']['event']['text']
+    sent_from = message['payload']['event']['user']
+    
+    if sent_from == sender:
+        return
+    
+    print("CURRENT TEXT:", text)
+    
+    
+    # ACTIONS - Get the first action and ask for more.
+    if text.lower() != 'training time' and training_time == True:
+        if action_received == False:
+            action = text.upper()
+            print(action)
+            send_message(channel, "Ok, let's call this action `{}`. Now give me some training text!".format(action))
+            action_received = True # First action receieved.
+            
+        # Receive next action.
+        elif action_received == True:
+            action = text.upper()
+            print(action)
+            send_message(channel, "OK, I've got it! what else?")
+        
+        # Finsih training if requested.
+        else:
+            send_message(channel, "OK, I'm finished training")
+    
+    # TRAINING TIME - start training upon user request!
+    elif text.lower() == 'training time' and training_time == False:
+        # Respond asking for action name using requests.posts method.
+        send_message(channel, "OK, I'm ready for training. What NAME should this ACTION be?")
+                
+        # Enable training mode!
+        training_time = True
+
+    
 
 def on_error(ws, error):
     print(error)
@@ -29,18 +96,18 @@ def on_close(ws):
 
 def on_open(ws):
     def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            ws.send("Hello %d" % i)
-        time.sleep(1)
-        #ws.close()
+        # for i in range(3):
+        #     time.sleep(1)
+        #     ws.send("Hello %d" % i)
+        # time.sleep(20)
+        # ws.close()
         print("thread terminating...")
     thread.start_new_thread(run, ())
 
 
 if __name__ == "__main__":
     websocket.enableTrace(False)
-    headers = {"Content-type":"application/x-www-form-urlencoded", "Authorization": "Bearer xapp-1-A02GFLAUZ0T-2552947742742-90bd24024278ee7f8e78f8b59a2f0fd544ab527688c7b25c2e7d2060dfa81153"}
+    headers = {'Content-type':'application/x-www-form-urlencoded', 'Authorization':'Bearer xapp-1-A02GFLAUZ0T-2578743379270-bf9c748d4bb04a350f475e17fca14182d0b565ed4093d68da04b4c812187466b'}
     url = requests.post('https://slack.com/api/apps.connections.open', headers=headers).json()["url"]
     ws = websocket.WebSocketApp(url,
                               on_message = on_message,
@@ -48,3 +115,4 @@ if __name__ == "__main__":
                               on_close = on_close)
     ws.on_open = on_open
     ws.run_forever()
+        
