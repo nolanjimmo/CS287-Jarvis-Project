8ee8d23e0cdb70ca10174ba3245e95f2501cff60
nolanjimmo <barcafan12345@gmail.com>
2021-10-12 12:15:10 -0400

    Jarvis is pretty much done!
    Everything is looking pretty good, Tucker will be populating the database this afternoon

--- jarvis.py
+++ jarvis.py
@@ -14,41 +14,42 @@ APP_TOKEN = lines[1].strip()
 import websocket
 import requests
 import json
+import time
 from dataInsertion import addVals
-
 
 try:
     import thread
 except ImportError:
     import _thread as thread
-import time
 
-##Global Variables for training mode switch
+
+# Global Variables for training mode switch
 training_time = False # Training Mode Switch
 action_received = False
 action = ''
 
 def send_message(channel, text):
-    data = {
-        'token': API_TOKEN,
-        'channel': channel,
-        'text': text
-        }
+    data = {'token': API_TOKEN,
+            'channel': channel,
+            'text': text}
     requests.post(url='https://slack.com/api/chat.postMessage', data=data)
     
 
 def on_message(ws, message):
+    
+    # Get global variables
     global training_time
     global action_received
     global action
     text = ''
     
-    #This is the user value for when our bot sends messsages
-    #We will be using this to make sure Jarvis doesn't respond to itself
-    sender = 'U02GCL03FSR'
+    # This is the user value for when our bot sends messsages
+    # We will be using this to make sure Jarvis doesn't respond to itself
+    bot = 'U02GCL03FSR'
 
     # Use envelope_id field to send a response of acknowledgement back to Slack.
     message = json.loads(message)
+    print(message['payload']['event']['text'])
     envelope_id = message['envelope_id']
     resp = {'envelope_id': envelope_id}
     ws.send(str.encode(json.dumps(resp)))
@@ -57,48 +58,43 @@ def on_message(ws, message):
     text = message['payload']['event']['text']
     sent_from = message['payload']['event']['user']
     
-    #input message validation, making sure that Jarvis does not respond to
-    #itself, and that the message text is not empty
-    if sent_from == sender:
+    if sent_from == bot:
         return
     if text == '':
         return
     
-    print("CURRENT TEXT:", text)
-    print(action_received)
+    # print("CURRENT TEXT:", text)
     
     # ACTIONS - Get the first action and ask for more.
-    if text.lower() != 'training time' and text.lower() != "done" and training_time == True:
-        if action_received == False:
+    if text.lower() != 'training time' and training_time == True:
+        if action_received == False and text.lower() != 'done':
             action = text.upper()
-            print(action)
             send_message(channel, "Ok, let's call this action `{}`. Now give me some training text!".format(action))
             action_received = True # First action receieved.
             
         # Receive next action.
-        elif action_received == True:
-            #action = text.upper()
+        elif action_received == True and text.lower() != 'done':
             print(f"Entering {action}: {text}     in to database")
-            addVals(text, action)
             send_message(channel, "OK, I've got it! what else?")
-    
+            addVals(text, action)
+        
+        # Finish training if requested.
+        else:
+            action_received = False
+            training_time = False
+            action = ''
+            send_message(channel, "OK, I'm finished training")
     
     # TRAINING TIME - start training upon user request!
     elif text.lower() == 'training time' and training_time == False:
+        
         # Respond asking for action name using requests.posts method.
         send_message(channel, "OK, I'm ready for training. What NAME should this ACTION be?")
                 
         # Enable training mode!
         training_time = True
+            
         
-    #finish training time on keyword "done"
-    elif text.lower() == "done":
-        send_message(channel, "OK, I'm finished training")
-        training_time = False
-        action_received = False
-        action = ''
-
-    
 
 def on_error(ws, error):
     print(error)
@@ -113,13 +109,13 @@ def on_open(ws):
         #     ws.send("Hello %d" % i)
         # time.sleep(20)
         # ws.close()
-        print("thread starting...")
+        print("Thread starting...")
     thread.start_new_thread(run, ())
 
 
 if __name__ == "__main__":
     websocket.enableTrace(False)
-    headers = {'Content-type':'application/x-www-form-urlencoded', 'Authorization':f'Bearer {APP_TOKEN}'}
+    headers = {'Content-type':'application/x-www-form-urlencoded', 'Authorization': f'Bearer {APP_TOKEN}'}
     url = requests.post('https://slack.com/api/apps.connections.open', headers=headers).json()["url"]
     ws = websocket.WebSocketApp(url,
                               on_message = on_message,
@@ -127,4 +123,3 @@ if __name__ == "__main__":
                               on_close = on_close)
     ws.on_open = on_open
     ws.run_forever()
-        
