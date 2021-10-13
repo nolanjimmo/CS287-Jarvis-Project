0bbe11df7402c11a712c9b2c4c0f9b5d3d7b53df
nolanjimmo <barcafan12345@gmail.com>
2021-10-12 20:11:00 -0400

    Copied data insertion function
    Copied data insertion function to the jarvis.py file

--- jarvis.py
+++ jarvis.py
@@ -2,7 +2,7 @@
 # Nolan Jimmo, Tucker Paron, Katey Forsyth, Chris O'Neil
 
 # TOKENS
-
+# Token Reading
 with open("tokens.txt", 'r') as infile:
     lines = infile.readlines()
 infile.close()
@@ -15,13 +15,30 @@ import websocket
 import requests
 import json
 import time
-from dataInsertion import addVals
 
 try:
     import thread
 except ImportError:
     import _thread as thread
 
+"""
+addVals adds values to the created sql table.
+param msgTxt is the message in it's entirety, action is the action that jarvis
+should take ie. WEATHER, TIME etc 
+Call each time a new training question is asked.
+"""
+import sqlite3
+
+def addVals(msgTxt, action):
+    
+    try:
+        conn = sqlite3.connect("jarvis.db")
+    except:
+        print("error connecting")
+    c = conn.cursor()
+    c.execute("INSERT INTO training_data (txt,action) VALUES (?, ?);", (msgTxt, action))
+    conn.commit() # save (commit) the changes
+    conn.close
 
 # Global Variables for training mode switch
 training_time = False # Training Mode Switch
