8a7ac6fdc64371fec4b67c1e9e2af3b492e831b1
Chris <christopher.oneil@uvm.edu>
2021-11-11 13:52:41 -0500

    Test of events class

---
+++ project2/event_test.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 10 14:59:01 2021
+
+@author: christopheroneil
+"""
+import datetime
+from Event import Event
+schedule = []
+
+while True:
+    
+    date_time = input("What is the date and time? ")
+    event_class = input("What is the event for? ")
+    event_description = input("What is the event? ")
+    
+    if len(schedule) == 0:
+        schedule.append(Event(date_time, event_class, event_description))
+    
+    if len(schedule) > 0:
+        for event in schedule:
+            if event == Event(date_time, event_class, event_description):
+                print("There is conflicting times with this new event with: ")
+                print(event)
+                continue
+            else:
+                schedule.append(Event(date_time, event_class, event_description))
+    
+    flag = input("Want to continue: yes or no? ")
+    
+    if flag[0].lower() == 'n':
+        break
+
+
