066939748e4462615aa9133ca603c46921ae2621
Christheoneoneil <78617902+Christheoneoneil@users.noreply.github.com>
2021-12-05 14:12:51 -0500

    Update data_wrangling.py
    finished data wrangling

--- final_project/data_wrangling.py
+++ final_project/data_wrangling.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Thu Dec  2 13:53:55 2021
 
@@ -11,6 +9,7 @@ Reading, compiling, and cleaning the three data sets
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from collections import Counter
 def load_data(filename):
     """ 
     load_data loads csv files to data frames
@@ -84,8 +83,7 @@ def merge_data(su_pop, life_expectancy, happiness_frame):
     
     # Create new data frame with only the countries contained in su_pop
     country_indexed = life_expectancy.set_index("Country")
-    for country in list(country_difference):
-        country_indexed = country_indexed.drop(country)
+    country_indexed = country_indexed.drop(list(country_difference))
     
     # Drop the population column from the dataframe because the column
     # had very large discrepancies
@@ -108,8 +106,7 @@ def merge_data(su_pop, life_expectancy, happiness_frame):
     countries_2015_difference = happiness_frame_countries.difference(final_suicdes_countries)
     
     country_2015_indexed = happiness_frame.set_index("country")
-    for country in list(countries_2015_difference):
-        country_2015_indexed = country_2015_indexed.drop(country)
+    country_2015_indexed = country_2015_indexed.drop(list(countries_2015_difference))
     
     final_2015_frame = country_2015_indexed.merge(final_suicdes_frame_2015,
                                                   how = "left", left_on = ["country", "year"],
@@ -134,12 +131,12 @@ def explore_data(final_suicides, final_2015):
     invalid_hist_list = []
     # Initialize a dictionary to keep track of missing values 
     nan_dict = {}
-    frame_list = [final_suicides, final_2015]
+    frame_list = [final_2015, final_suicides]
     frame_num = 1
     
     # For each frame graph the histogram and count nans.
     for frame in frame_list:
-        
+        nan_dict = {}
         for col in frame.columns:
             if (col != "year" and col != "country"):
                 nan_count = frame[col].isnull().values.sum()
@@ -154,33 +151,58 @@ def explore_data(final_suicides, final_2015):
                 except ValueError:
                     invalid_hist_list.append(col)
         frame_num += 1
-    
+        
         print(nan_dict)
         print(invalid_hist_list)
+        
+    
+    # Alcohol was dropped because it only had two values for 2015 
+    final_2015.drop('Alcohol', axis = 1, inplace = True)
+    # Total expenditure was dropped because it had no values 
+    final_2015.drop('Total expenditure', axis = 1, inplace = True)
+    # Precentave expenditure as dropped because it was either zero or NA for 2015
+    final_2015.drop('percentage expenditure', axis = 1, inplace = True)
+    
+    """
+    Suicudes frame cleaning:
+    Find the top countries with the highest NaN counts by iterating through
+    and detecting na values for each column. Once that is found append them to 
+    a list. Then use counter to count each countries na columns. A list
+    of the highest na countries was found then they were dropped from the final
+    frame. 
+    """
+    countries_with_na_cols = []
+    for country in final_suicides.country.unique():
+        for column in nan_dict.keys():
+            if final_suicides[final_suicides['country'] == country][column].isnull().values.any():
+                countries_with_na_cols.append(country)
+    
+    print(Counter(countries_with_na_cols))
+    top_countries_na = ['Aruba', 'Czech Republic', 'Puerto Rico', 
+                        'Saint Vincent and Grenadines', 'San Marino', 
+                        'United Kingdom', 'United States']
+    
+    final_suicides.set_index("country", inplace = True)
+    final_suicides.drop(top_countries_na, axis = 0, inplace = True)
+    
+    save_to_csv(final_suicides, final_2015)
+
+def save_to_csv(f_s_frame, f_2015_frame):
+    """ 
+    save to csv takes the merged data frames and saves them to a csv
+   
+    Parameters
+    ----------
+    final suicides frame 
+    final 2015 frame
+
+    Returns
+    -------
+    None
+    """
+    
+    f_s_frame.to_csv('merged_data/merged_suicide.csv')
+    f_2015_frame.to_csv('merged_data/merged_2015.csv')
+    
+
 data_extraction()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
